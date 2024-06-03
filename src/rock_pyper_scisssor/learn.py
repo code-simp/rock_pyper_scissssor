8c9a724165cf2bda50281eb2c84828c2d218ac2b
code-simp <itanupad@gmail.com>
2024-06-04 00:05:28 +0530

    changed the logic to add tie

---
+++ src/rock_pyper_scisssor/learn.py
@@ -0,0 +1,12 @@
+"""
+This file has all the logic for learning methods to beat the player
+"""
+
+
+#################################################
+#                                               #
+# A simple probability based learning algorithm #
+#                                               #
+#################################################
+
+# This section has a try hard algorithm that learns what the user
--- src/rock_pyper_scisssor/play.py
+++ src/rock_pyper_scisssor/play.py
@@ -6,7 +6,16 @@ import time
 import sys
 import random
 import getch
-import string
+
+
+KEY_MAP = {
+    'r': 'Rock!!',
+    'p': 'Paper!!',
+    's': 'Scissors!!'
+}
+
+SLEEP_ENABLED = True
+
 
 
 def write_to_screen(text):
@@ -35,46 +44,81 @@ def randomize_outcome():
     return random.choice(["Rock!!", "Paper!!", "Scissors!!"])
 
 
+def user_won(computer_generated, user_input):
+
+    if computer_generated == user_input:
+        return False
+
+    elif computer_generated == "Rock!!" and user_input == "Paper!!":
+        return True
+
+    elif computer_generated == "Paper!!" and user_input == "Scissors!!":
+        return True
+
+    elif computer_generated == "Scissors!!" and user_input == "Rock!!":
+        return True 
+
+    else:
+        return False
+
+
+def sleep_for(time_in_seconds):
+    global SLEEP_ENABLED
+
+    if SLEEP_ENABLED:
+        time.sleep(time_in_seconds)
+
+
+
 def level_1():
-    line_seperated_text("Welcome to level 1., Good Luck")
+    line_seperated_text("Welcome to level 1., Good Luck\n")
     write_to_screen("Pick from Rock-Paper-Scissors, I say Rock, Paper, Scissors... Shoot! and we go!\n\n")
 
-    time.sleep(3)
+    sleep_for(3)
 
     sys.stdout.write("\rRock...")
     sys.stdout.flush()
-    time.sleep(1)
+    sleep_for(1)
 
     sys.stdout.write("\rPaper...")
     sys.stdout.flush()
-    time.sleep(1)
+    sleep_for(1)
 
     sys.stdout.write("\rScissors...")
     sys.stdout.flush()
-    time.sleep(1)
+    sleep_for(1)
 
     sys.stdout.write("\rShooooooot!")
     sys.stdout.flush()
-    time.sleep(1)
+    sleep_for(1)
 
-    highlighted_text(randomize_outcome())
+    computer_generated = randomize_outcome()
+
+    highlighted_text(computer_generated)
+
+    return computer_generated
 
 
 def play_game():
     while True:
         
-        level_1()
+        computer_generated = level_1()
 
-        player_won = str(read_single_key("\n\nDid you win? (y/n) ")).lower() == "y"
+        player_input = KEY_MAP[str(read_single_key("\n\nWhat was your choice? r / p / s ")).lower()]
+
+        player_won = user_won(computer_generated, player_input)
 
         if player_won:
-            line_seperated_text("Good Job. You won !")
+            line_seperated_text("\nGood Job. You won !")
+
+        elif player_input == computer_generated:
+            line_seperated_text("\nIt's a tie")
 
         else:
             line_seperated_text("\nAwww! Better luck next time")
         
 
-        play_again = str(read_single_key("\n\nWould you like to play again? (y/n) \n\n")).lower() == "y"
+        play_again = str(read_single_key("\nWould you like to play again? (y/n) \n\n")).lower() == "y"
 
         if not play_again:
             break
